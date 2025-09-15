8b5493a20957063d28c552a8e0a087f58f6f691a
IR101D <somefkingadress@gmail.com>
2025-09-15 20:31:21 +0100

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -8,6 +8,9 @@ export interface RowElement {
 
 import { RowId , RowElement} from "./interface"
 
-export function insertRow(row: RowElement): RowID;
-export function deleteRow(row : RowID): void;
-export function updateRow(rowId: RowID, row: RowElement): RowID;
+declare function insertRow(row: RowElement): number;
+//export function insertRow(row: RowElement): RowID;
+//export function deleteRow(row : RowID): void;
+declare function deleteRow(rowId: RowID): void
+declare function updatedRow: RowElement ;
+CRUD.updateRow(newRowID, updatedRow);
